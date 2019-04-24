ebc0f24e84041f73112cfcf7ea309392fea88985
Mateusz <matpiwowarski7@gmail.com>
2019-04-24 17:42:36 +0200

    Blocking the player from leaving the map

--- BackyardGame/BackyardGame/Entity.cpp
+++ BackyardGame/BackyardGame/Entity.cpp
@@ -19,13 +19,17 @@ void Entity::move(const double &dt, const double xDirection, const double yDirec
 void Entity::update(const double & dt)
 {
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) || sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-		this->move(dt, -1.f, 0.f);
+		if(this->shape.getPosition().x > 0 )
+			this->move(dt, -1.f, 0.f);
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) || sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-		this->move(dt, 1.f, 0.f);
+		if (this->shape.getPosition().x < 768)
+			this->move(dt, 1.f, 0.f);
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) || sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-		this->move(dt, 0.f, -1.f);
+		if (this->shape.getPosition().y > 0)
+			this->move(dt, 0.f, -1.f);
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) || sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-		this->move(dt, 0.f, 1.f);
+		if (this->shape.getPosition().y < 545)
+			this->move(dt, 0.f, 1.f);
 }
 
 void Entity::render(sf::RenderTarget * target)
